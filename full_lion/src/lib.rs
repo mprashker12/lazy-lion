1530c7da9eecb07ae04259743df03b8f24badc0f
MatthewPrashker <prashker.m@northeastern.edu>
2023-05-10 13:53:54 -0400

    roars

--- full_lion/src/lib.rs
+++ full_lion/src/lib.rs
@@ -1,6 +1,2 @@
-use square_reed_solomon::prover;
 pub mod node;
 
-pub trait AbstractChannel {}
-
-pub trait ChannelSender<C: AbstractChannel> {}
--- full_lion/src/node.rs
+++ full_lion/src/node.rs
@@ -1,16 +1,31 @@
-use ark_ec::pairing::Pairing;
 use square_reed_solomon::{prover::RsSquareProver, rs_square::RsSquare};
 
 use rs_merkle::{MerkleTree, algorithms::Sha256, Hasher};
+use ark_ec::pairing::Pairing;
 use ark_ff::PrimeField;
 use anyhow::Result;
 
+use std::sync::Mutex;
+use std::sync::Arc;
+
+
+use tokio::net::{TcpSocket, TcpStream};
+
 pub struct FullLionNode<E : Pairing, H : Hasher> {
     square: RsSquare<E::ScalarField>,
-    prover: RsSquareProver<E, H>,
+    inner: FullLionNodeInner<E, H>,
+}
+
+#[derive(Clone)]
+pub struct FullLionNodeInner<E: Pairing, H: Hasher> {
+    prover: Arc<Mutex<RsSquareProver<E, H>>>,
 }
 
 impl<E : Pairing, H : Hasher> FullLionNode<E, H> {
+
+    pub fn new(data: &[u8], stream: TcpStream) -> Self {
+        todo!();
+    }
 
 
     pub async fn run(&mut self) -> Result<()> {
